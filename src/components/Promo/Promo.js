be4406e973b6fd8170f52bb737577613bf73fb81
elena1983-zinatylina <tortdekor2016@yandex.ru>
2023-07-13 11:53:14 +0300

    работа с макетом в перфект пиксель

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import './Promo.css'
+import './Promo.css';
+import promo from '../../images/landing-logo.svg';
 
-function Promo() {
+const Promo = () => {
   return (
-    <section className="promo">
-      <div className="promo__img"></div>
-      <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+    <section className='promo'>
+      <div className='promo__container'>
+        <h1 className='promo__title'>
+          Учебный проект студента факультета Веб-разработки.
+        </h1>
+      <img className='promo__img' src={promo} alt='Логотип Промо.' />
+      </div>
     </section>
-  );
-}
+  )
+};
 
 export default Promo;
