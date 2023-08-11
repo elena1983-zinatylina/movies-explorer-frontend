57fa623e188293e5e8f7ed568249a6d6cfd92e19
elena1983-zinatylina <tortdekor2016@yandex.ru>
2023-08-11 23:47:54 +0300

    навтаб удалила

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Main.css";
 import Header from "../Header/Header";
 import Promo from "../Promo/Promo";
-import NavTab from "../NavTab/NavTab";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
 import AboutMe from "../AboutMe/AboutMe";
@@ -15,7 +14,6 @@ function Main({ loggedIn }) {
       <Header loggedIn={loggedIn}/>
       <main className="main">
         <Promo />
-        <NavTab />
         <AboutProject />
         <Techs />
         <AboutMe />
