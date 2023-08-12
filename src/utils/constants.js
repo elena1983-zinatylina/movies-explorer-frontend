d87f1abaafff0e27d2c3a58c7ad3604bd5c71c1f
elena1983-zinatylina <tortdekor2016@yandex.ru>
2023-08-12 21:42:25 +0300

    savedmoves

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,4 +13,18 @@ export const DISPLAY_SETTINGS = {
         start: 5,
         load: 1
     }
+}
+
+export const convertDuration = (number) => {
+    const minutes = number % 60;
+    const hours = (number - minutes) / 60;
+    if (hours === 0) {
+       return `${minutes}м`;
+    } else if (minutes === 0) {
+       return `${hours}ч`;
+    } else {
+       return `${hours}ч ${minutes}м`;
+    }
  };
+ 
+export const SHORT_MOVIE_DURATION = 40;
